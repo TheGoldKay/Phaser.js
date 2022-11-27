76318a6f787ebccbad0ee509d47f1fe1f6dfa5a8
TheGoldKay <jhonnatanwfc@live.com>
2022-11-27 14:43:10 -0300

    event handling

--- snake/js/game.js
+++ snake/js/game.js
@@ -2,19 +2,35 @@ let gameScene = new Phaser.Scene('Game');
 
 gameScene.init = function(){
     this.snake_dir = "left";
+
 }
 
 gameScene.preload = function(){
   this.load.image('head', "darkgray.png");
+  this.left = this.input.keyboard.addKey('a');
+  this.right = this.input.keyboard.addKey('d');
+  this.up = this.input.keyboard.addKey('W');
+  this.down = this.input.keyboard.addKey('s');
 }
 
 gameScene.create = function(){
   this.head = this.add.sprite(40, 40, 'head');
+  this.input.keyboard.on('keydown_W', this.update, this);
 }
 
 gameScene.update = function(){
-  this.head.x += 1;
-  this.head.y += 1;
+  if(this.left.isDown){
+    this.head.x -= 1;
+  }
+  else if(this.right.isDown){
+    this.head.x += 1;
+  }
+  else if(this.up.isDown){
+    this.head.y -= 1;
+  }
+  else if(this.down.isDown){
+    this.head.y += 1;
+  }
 }
 
 
